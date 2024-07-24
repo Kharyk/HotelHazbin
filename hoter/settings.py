f3df2b684db07859a22be40fa964b9d42f093349
alinakharyk <alinakharyk@gmail.com>
2024-07-24 22:02:04 +0300

    commited files

--- hoter/settings.py
+++ hoter/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
